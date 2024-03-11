21e07b10f05190bcd624f426d40cc6ee82cb13b0
[DudiyMaksim] <[maksdudiya25@gmail.com]>
2024-03-11 20:00:31 +0200

    Add project files.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    ///*task 1*/
+    //cout << "==I have many pencils==\nI have many pencils :\nRed and green and blue.\nI can draw a picture\nAnd give it to you.\nI can draw my mother, \nI can draw my father, \nI can draw my sister, \nAnd my little brother.\n\n\tJustSchool\n";
+    ///*task 2*/
+    //cout << "Every" << "\n";
+    //cout << "\tHunter" << "\n";
+    //cout << "\t\tWants" << "\n";
+    //cout << "\t\t\tTo know" << "\n";
+    //cout << "\t\t\t\t Where" << "\n";
+    //cout << "\t\t\t\t\tDoes Pheasant" << "\n";
+    //cout << "\t\t\t\t\t\t\tSit" << "\n\n";
+    ///*task 3*/
+    //cout << "Sell tsutsutsenyat.\nBreed: Rottweiler.\n\n...\n\ntel.:222-22-22\n\n222\t| 222 |\n\n22\t| 22  |\n\n22\t| 22  |\n\ni t.d.";
+}
